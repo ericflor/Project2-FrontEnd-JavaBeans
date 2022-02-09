3b7f2c60a48934ffa3bb9a160f8cb172e08f6b3a
Eric Florence <ericflorence.dev@gmail.com>
2022-02-09 13:02:36 -0700

    working on imdb api calls

--- project2/src/app/components/favorites/favorites.component.ts
+++ project2/src/app/components/favorites/favorites.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-favorites',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
 
-  constructor() { }
+  url:string = "https://imdb-api.com/en/API/SearchMovie/k_muze8ch7/inception";
+  movies: any;
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    console.log("alkdf");
+    this.http.get(this.url).subscribe(data=>{
+      console.log(data);
+      
+      this.movies = data as any;
+      
+    });
+    console.log(this.movies);
   }
 
 }

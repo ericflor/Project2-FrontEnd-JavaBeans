3e6d636e2e6fe5097031b078bcc2d3e27feec976
Elias Calagiu <eliasc81@gmail.com>
2022-02-09 19:52:16 -0800

    getting top 250 movies from api

--- project2/src/app/decisions/decisions.component.ts
+++ project2/src/app/decisions/decisions.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { DecisionsService } from '../services/decisions.service';
+
 
 @Component({
   selector: 'app-decisions',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DecisionsComponent implements OnInit {
 
-  constructor() { }
+  url:string = "https://imdb-api.com/en/API/Top250Movies/k_muze8ch7/";
+
+  // randomize which indices from response we receive 
+  movieArray: any = [];
+
+  constructor(private decisionsService:DecisionsService, private http:HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  // populate with array of ten movie objects from Response body of API call to imdbapi 
+  getMovies(movieArray: any[]){
+    this.movieArray = movieArray;
+
+    this.http.get(this.url).subscribe(data=>{
+      this.movieArray = data;
+      console.log(this.movieArray);
+      
+    })
+
   }
 
 }

9221a666578e038d871fca82597e3e752a23ed56
Jessie Guadarrama <jesguadarrama98@gmail.com>
2022-02-14 09:55:48 -0800

    created functionality to loop through top 250 movies and return two lists of 10. One list returns the whole movie object, the other retur returns just IDs

--- project2/src/app/decisions/decisions.component.ts
+++ project2/src/app/decisions/decisions.component.ts
@@ -26,10 +26,38 @@ export class DecisionsComponent implements OnInit {
 
     this.http.get(this.url).subscribe(data=>{
       this.movieArray = data;
-      console.log(this.movieArray);
-      
+
+      var items: any = this.movieArray["items"] // returns items key in movieArray object
+
+      //console.log(items)
+        this.randomizeList(items)
     })
 
   }
 
+  //create a randomized list of 10 movies
+ randomizeList(film:any) { 
+   for (var i = film.length - 1; i > 0; i--) {  //loop through each movie
+     
+     var j = Math.floor(Math.random() * (i + 1)); //element from array is chosen
+     var temp = film[i];  //element is picked
+     film[i] = film[j];   //current element is swapped with random
+     film[j] = temp;      //new element is picked
+     //console.log(film[i].id);
+     
+   }
+   var tenMovies = film.slice(0, 10); //reduce randomized list to 10 movies
+   //console.log(tenMovies);
+
+ var moiveIdArray = new Array(); //create an array to store just IDs
+   for (var i = 0; i < 10; i++){ //loop through 10 movies
+     var movieIds = tenMovies[i].id
+     //grab the id and push to array
+    var list = moiveIdArray.push(movieIds);
+  
+   }
+   console.log(moiveIdArray)
+ }
+    
+  
 }

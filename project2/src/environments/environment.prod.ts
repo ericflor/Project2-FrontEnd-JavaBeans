452a74e3a6147964e962e59640f21bf3c5fea70f
JustinBreaux <96553121+JustinBreaux@users.noreply.github.com>
2022-02-14 16:31:03 -0500

    changed port in environment production variable to 7000

--- project2/src/environments/environment.prod.ts
+++ project2/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverURL: "http://54.215.40.51:8083/data/",
+  serverURL: "http://54.215.40.51:7000/data/",
   apiURL: "https://imdb-api.com/en/API/"
 };

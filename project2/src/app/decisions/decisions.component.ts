1a51f44e51ba83ff56ce043d121ba5248d48a92f
Jessie Guadarrama <jesguadarrama98@gmail.com>
2022-02-16 07:35:34 -0800

    ready to pull feature branch

--- project2/src/app/decisions/decisions.component.ts
+++ project2/src/app/decisions/decisions.component.ts
@@ -18,17 +18,11 @@ export class DecisionsComponent implements OnInit {
   items: any;
   tenMovies: any;
   movieTitleArray: any;
-<<<<<<< HEAD
-  oneMovieArray: any=[];
-  favoritesArray: any=[];
-  url:string = "https://imdb-api.com/en/API/Top250Movies/k_06em724z/";
-=======
   likedArray: any = [];
   dislikedArray: any =[];
   favoritesArray: any = [];
   oneMovieArray: any = [];
   url: string = "https://imdb-api.com/en/API/Top250Movies/k_06em724z/";
->>>>>>> main
 
 
 
@@ -42,13 +36,6 @@ export class DecisionsComponent implements OnInit {
   // populate with array of ten movie objects from Response body of API call to imdbapi 
   getMovies(movieArray: any[]) {
     this.movieArray = movieArray;
-<<<<<<< HEAD
-    
-    this.http.get(this.url).subscribe(data=>{
-    this.movieArray = data;
-    this.items = this.movieArray["items"] // returns items key in movieArray object
-    this.randomizeList(this.items)
-=======
 
     this.http.get(this.url).subscribe(data => {
       this.movieArray = data;
@@ -57,7 +44,6 @@ export class DecisionsComponent implements OnInit {
 
       //console.log(items)
       this.randomizeList(this.items)
->>>>>>> main
     })
   }
 
@@ -69,77 +55,17 @@ export class DecisionsComponent implements OnInit {
       var temp = film[i];  //element is picked
       film[i] = film[j];   //current element is swapped with random
       film[j] = temp;      //new element is picked
-<<<<<<< HEAD
-    }
-    this.tenMovies = film.slice(0, 10); //reduce randomized list to 10 movies
-    console.log(this.tenMovies)
-    this.getIMDBIds(this.tenMovies);
-=======
 
 
     }
     this.tenMovies = film.slice(0, 10); //reduce randomized list to 10 movies
-    console.log(this.tenMovies);
+    //console.log(this.tenMovies);
     this.getIMDBTitles(this.tenMovies);
->>>>>>> main
     this.getOneMovie(this.tenMovies);
-    // this.addToFavorites(this.tenMovies);
+    this.addToFavorites(this.tenMovies);
   }
 
 
-<<<<<<< HEAD
-  getIMDBIds(Ids:any){
-    this.movieTitleArray = new Array(); //create an array to store just IDs
-   for (var i = 0; i < 10; i++){ //loop through 10 movies
-    var movieTitles = this.tenMovies[i].title
-   
-    let list = this.movieTitleArray.push(movieTitles);
-   }
-   //console.log(this.movieTitleArray)
- }
-  
- getOneMovie(Movie:any){
-  this.oneMovieArray = new Array();
-    if(this.tenMovies.length>0){
-      let likeBtn:any = document.getElementById("likeBtn");
-      let dislikeBtn: any = document.getElementById("dislikeBtn");
-      
-      likeBtn.addEventListener("click", ()=>{ 
-        this.oneMovieArray = []
-        let oneMovie = this.tenMovies.shift();  
-        console.log(oneMovie);
-        let nextMovie = this.oneMovieArray.push(oneMovie);
-          
-      })
-      dislikeBtn.addEventListener("click", ()=>{
-          
-        this.oneMovieArray = []
-        let oneMovie = this.tenMovies.shift();
-      
-        //console.log(oneMovie);
-        let nextMovie = this.oneMovieArray.push(oneMovie);
-        
-      })
-    }
-  }
-
-
- addToFavorites(Movie:any){
-  this.favoritesArray = new Array();
-  if(this.tenMovies.length>0){
-    let favBtn: any = document.getElementById("favBtn");
-    favBtn.addEventListener("click", () =>{
-    
-    this.favoritesArray = [];
-    let currentMovie = this.oneMovieArray[0].title;
-    let favoriteMovie = this.favoritesArray.push(currentMovie);
-    console.log(favoriteMovie);
-    
-    })
-  } 
-  }
-}
-=======
   getIMDBTitles(Ids: any) {
     this.movieTitleArray = new Array(); //create an array to store just IDs
     for (var i = 0; i < 10; i++) { //loop through 10 movies
@@ -157,64 +83,50 @@ export class DecisionsComponent implements OnInit {
       let dislikeBtn: any = document.getElementById("dislikeBtn");
 
       likeBtn.addEventListener("click", () => {
-
+        this.oneMovieArray = []
         // this.likedArray = []
         let oneMovie = this.tenMovies.shift();
 
-        console.log(oneMovie);
+        //console.log(oneMovie);
 
         let nextMovie = this.likedArray.push(oneMovie);
         let appendedMovie = this.oneMovieArray.push(oneMovie);
-        // console.log(nextMovie);
+        console.log(nextMovie);
         
         console.log(this.likedArray);
-        console.log(this.tenMovies);
+       // console.log(this.tenMovies);
         
 
       })
       dislikeBtn.addEventListener("click", () => {
-
+        this.oneMovieArray = []
         // this.dislikedArray = []
         let oneMovie = this.tenMovies.shift();
 
-        console.log(oneMovie);
+        //console.log(oneMovie);
         let nextMovie = this.dislikedArray.push(oneMovie);
         let appendedMovie = this.oneMovieArray.push(oneMovie);
         console.log(this.dislikedArray);
-        console.log(this.tenMovies)
-
-
-
-
+        //console.log(this.tenMovies)
       })
     }
   }
 
 
-  //  addToFavorites(Movie:any){
-  //   this.favoritesArray = new Array();
-  //   if(this.tenMovies.length>0){
-  //     let favBtn: any = document.getElementById("favBtn");
-  //   favBtn.addEventListener("click", () =>{
+   addToFavorites(Movie:any){
+    this.favoritesArray = new Array();
+    if(this.tenMovies.length>0){
+      let favBtn: any = document.getElementById("favBtn");
+      favBtn.addEventListener("click", () =>{
 
-  //     this.favoritesArray = [];
-  //     let currentMovie = this.dislikedArray[0].title;
-  //     let favoriteMovie = this.favoritesArray.push(currentMovie);
-  //     console.log(favoriteMovie);
+      this.favoritesArray = [];
+      let currentMovie = this.oneMovieArray[0].title;
+      let favoriteMovie = this.favoritesArray.push(currentMovie);
+        console.log(favoriteMovie);
+        console.log(this.favoritesArray);
 
-  //   })
-  //   }
+    })
+    }
 
-  // }
+  }
 }
-
-
-
-
-
-
-
-
-
-
->>>>>>> main

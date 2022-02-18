1c53e4d031d84ac68af64923ce82d2569226ae9a
JustinBreaux <96553121+JustinBreaux@users.noreply.github.com>
2022-02-17 19:08:46 -0500

    finished functionality of group feature

---
+++ project2/src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  url:string = environment.serverURL;
+
+  constructor(private http:HttpClient) { }
+
+  getCurrentUser(){
+    return this.http.get<User>(this.url+"user/current", {withCredentials:true, observe:'response'});
+  }
+}
